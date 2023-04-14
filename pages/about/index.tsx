83da2f04573c67bb12d7740789fb7ccd7a3d32c4
katsunariootomo <ka.mi.0512@icloud.com>
2023-04-14 20:16:22 +0900

    ポートフォリオ工夫完成

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,40 +1,40 @@
 import { Box, Flex, Heading, Text, Image } from "@chakra-ui/react";
 
 const About = () => {
-  return (
-    <Flex minH="100vh" alignItems="center">
-      <Box
-        maxW="md"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-        mx="auto"
-        my={10}
-        boxShadow="md"
-        bg="white"
-      >
-        <Box bg="gray.100" h="200px">
-          <Image src="./images/profile.jpg" alt="プロフィール画像" h="100%" />
-        </Box>
-        <Box p={6}>
-          <Heading as="h1" size="xl" mb={4}>
-            自己紹介
-          </Heading>
-          <Text fontSize="xl" mb={8}>
-            こんにちは、大供克成と申します。
-          </Text>
-          <Text fontSize="lg" lineHeight="tall">
-            私はWeb開発に興味を持っており、現在はフロントエンドエンジニアとして活躍しています。
-            以前は別の業界に勤めており、Web開発の経験は浅いですが、日々勉強を重ねてスキルアップに励んでいます。
-          </Text>
-          <Text fontSize="lg" lineHeight="tall" mt={8}>
-            このページでは、私が開発したプロジェクトやスキルについて紹介しています。
-            ぜひご覧いただければ幸いです。
-          </Text>
-        </Box>
-      </Box>
-    </Flex>
-  );
+return (
+<Flex minH="100vh" alignItems="center">
+<Box
+     maxW="md"
+     borderWidth="1px"
+     borderRadius="lg"
+     overflow="hidden"
+     mx="auto"
+     my={10}
+     boxShadow="md"
+     bg="white"
+   >
+<Box bg="gray.10" h="500px">
+<Image src="./images/profile.jpg" alt="プロフィール画像" h="100%" />
+</Box>
+<Box p={6}>
+<Heading as="h1" size="xl" mb={4}>
+かっくんの自己紹介
+</Heading>
+<Text fontSize="xl" mb={8}>
+こんにちは、かっくんです。
+</Text>
+<Text fontSize="lg" lineHeight="tall">
+大手外資系商社でエンジニアとして働いています。フロントエンド開発を中心に、業務システムの開発に従事しています。
+Web開発は比較的最近から始めたものの、日々の業務を通じてスキルアップに励んでいます。
+</Text>
+<Text fontSize="lg" lineHeight="tall" mt={8}>
+このページでは、私が開発したプロジェクトや得意とするスキルについて紹介しています。
+ぜひご覧いただければ幸いです。
+</Text>
+</Box>
+</Box>
+</Flex>
+);
 };
 
 export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,14 @@ const inter = Inter({ subsets: ["latin"] });
 function Home() {
   return (
     <Box className={styles.container}>
-      <Flex as="nav" align="center" justify="space-between" py={4} px={8}>
+      <Flex
+        as="nav"
+        align="center"
+        justify="space-between"
+        py={4}
+        px={8}
+        bg="gray.100"
+      >
         <Box>
           <Heading as="h1" size="lg" letterSpacing="-0.1rem">
             かっくんのポートフォリオ
@@ -31,81 +38,110 @@ function Home() {
           </Box>
         </Stack>
       </Flex>
-      <Box as="section" id="about" className={styles.aboutSection} px={8} py={16}>
-        <Heading as="h1" size="xl" mb={4} className={styles.heading}>
-          かっくんのポートフォリオ
-        </Heading>
-        <Text fontSize="lg" mb={8} className={styles.intro}>
-          はじめまして、私はかっくんです。このポートフォリオでは、私のプロジェクトやスキルを紹介します。
-        </Text>
-        <Box maxW="sm" mx="auto">
-          <Image
-            src="/profile.jpg"
-            alt="プロフィール画像"
-            width={200}
-            height={200}
-            borderRadius="full"
-            mb={8}
-            className={styles.profileImage}
-          />
-        </Box>
-        <Text fontSize="lg" mb={8} className={styles.profileBio}>
-          私はフロントエンド開発に興味を持っており、ReactやNext.jsなどのフレームワークを使用してWebアプリケーションを開発しています。
-        </Text>
+      <Box
+        as="section"
+        id="about"
+        className={styles.aboutSection}
+        px={8}
+        py={16}
+      >
+        <Flex alignItems="center">
+          <Box maxW="sm" mx="auto">
+            <Image
+              src="/images/profile2.jpg"
+              alt="プロフィール画像"
+              width={200}
+              height={200}
+              borderRadius="full"
+              mb={8}
+              className={styles.profileImage}
+            />
+          </Box>
+          <Box mx={8}>
+            <Heading as="h1" size="xl" mb={2} className={styles.heading}>
+              かっくん
+            </Heading>
+            <Text fontSize="xl" className={styles.profileBio}>
+              大手外資系商社で働くエンジニア。フロントエンド開発を中心に、業務システムの開発に従事している。
+            </Text>
+            <Text fontSize="xl" className={styles.profileBio}>
+              React, Next.js, TypeScriptを得意とし、最近はUI/UXの改善にも力を入れている。
+            </Text>
+          </Box>
+        </Flex>
       </Box>
-      <Box as="section" id="projects" className={styles.projectsSection} px={8} py={16}>
-        <Heading as="h2" size="lg" mb={8} className={styles.subheading}>
+      <Box
+        as="section"
+        id="projects"
+        className={styles.projectsSection}
+        px={8}
+        py={16}
+      >
+        <Heading as="h2" size="xl" mb={8} className={styles.subheading}>
           プロジェクト
         </Heading>
         <Stack spacing={8}>
           <Box>
-            <Heading as="h3" size="md" mb={4} className={styles.projectTitle}>
-              プロジェクト1
+            <Heading as="h3" size="lg" mb={2} className={styles.projectTitle}>
+              社内向け業務システムの開発
             </Heading>
             <Text fontSize="lg" className={styles.projectDescription}>
-              これはプロジェクト1の説明です。
-            </Text>
-          </Box>
-          <Box>
-            <Heading as="h3" size="md" mb={4} className={styles.projectTitle}>
-              プロジェクト2
-            </Heading>
-            <Text fontSize="lg"
-className={styles.projectDescription}>
-これはプロジェクト2の説明です。
-</Text>
-</Box>
-</Stack>
-</Box>
-<Box as="section" id="contact" className={styles.contactSection} px={8} py={16}>
-<Heading as="h2" size="lg" mb={8} className={styles.subheading}>
-お問い合わせ
-</Heading>
-<Text fontSize="lg" mb={8} className={styles.contactDescription}>
-ご質問やご要望があれば、お気軽にお問い合わせください。
-</Text>
-<Stack as="form" spacing={4} direction="column">
-<Box>
-<Input type="text" id="name" name="name" placeholder="お名前" required />
-</Box>
-<Box>
-<Input type="email" id="email" name="email" placeholder="メールアドレス" required />
-</Box>
-<Box>
-<Input as="textarea" id="message" name="message" placeholder="お問い合わせ内容" required />
-</Box>
-<Box>
-<Input type="submit" value="送信" className={styles.submitButton} />
-</Box>
-</Stack>
-</Box>
-<Box as="footer" className={styles.footer} px={8} py={4}>
-<Text fontSize="lg">
-© {new Date().getFullYear()} かっくん. All rights reserved.
-</Text>
-</Box>
-</Box>
-);
+              社員の効率的な業務遂行のため、社内システムの改修・新規開発を行いました。
+              フロントエンド開発にはReactとTypeScript、バックエンドにはNode.jsを使用しました。
+        従業員の出勤・勤怠管理、経費申請、業務報告書作成など、幅広い業務プロセスをカバーするシステムを構築しました。
+      </Text>
+      <Box>
+        <Heading as="h3" size="lg" mb={2} className={styles.projectTitle}>
+          オンラインショップの開発
+        </Heading>
+        <Text fontSize="lg" className={styles.projectDescription}>
+          ユーザーが商品を閲覧・購入できるオンラインショップを開発しました。
+          フロントエンド開発にはReact、バックエンド開発にはNode.jsとExpressを使用し、
+          決済処理にはStripe APIを導入しました。
+          商品検索やカート機能、注文履歴の確認など、一般的なECサイトに必要な機能を実装しました。
+        </Text>
+        </Box>
+      </Box>
+    </Stack>
+  </Box>
+
+  <Box
+    as="section"
+    id="contact"
+    className={styles.contactSection}
+    px={8}
+    py={16}
+  >
+    <Heading as="h2" size="xl" mb={8} className={styles.subheading}>
+      お問い合わせ
+    </Heading>
+    <Text fontSize="lg" mb={8} className={styles.contactDescription}>
+      ご質問やご要望があれば、お気軽にお問い合わせください。
+    </Text>
+    <Stack as="form" spacing={4} direction="column">
+      <Box>
+        <Input type="text" id="name" name="name" placeholder="お名前" required />
+      </Box>
+      <Box>
+        <Input type="email" id="email" name="email" placeholder="メールアドレス" required />
+      </Box>
+      <Box>
+        <Input as="textarea" id="message" name="message" placeholder="お問い合わせ内容" required />
+      </Box>
+      <Box>
+        <Input type="submit" value="送信" className={styles.submitButton} />
+      </Box>
+    </Stack>
+  </Box>
+
+  <Box as="footer" className={styles.footer} px={8} py={4}>
+    <Text fontSize="lg">
+      © {new Date().getFullYear()} かっくん. All rights reserved.
+    </Text>
+  </Box>
+    </Box>
+    
+    );
 }
 
 export default Home;
