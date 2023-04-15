026099a4ebf1500784bb1e53bc33f82e73dd9bdc
katsunariootomo <ka.mi.0512@icloud.com>
2023-04-16 02:41:39 +0900

    少し工夫した

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,40 +1,93 @@
-import { Box, Flex, Heading, Text, Image } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, Image, IconButton, Link } from "@chakra-ui/react";
+import { FaBars, FaPhone } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
 
 const About = () => {
-return (
-<Flex minH="100vh" alignItems="center">
-<Box
-     maxW="md"
-     borderWidth="1px"
-     borderRadius="lg"
-     overflow="hidden"
-     mx="auto"
-     my={10}
-     boxShadow="md"
-     bg="white"
-   >
-<Box bg="gray.10" h="500px">
-<Image src="./images/profile.jpg" alt="プロフィール画像" h="100%" />
-</Box>
-<Box p={6}>
-<Heading as="h1" size="xl" mb={4}>
-かっくんの自己紹介
-</Heading>
-<Text fontSize="xl" mb={8}>
-こんにちは、かっくんです。
-</Text>
-<Text fontSize="lg" lineHeight="tall">
-大手外資系商社でエンジニアとして働いています。フロントエンド開発を中心に、業務システムの開発に従事しています。
-Web開発は比較的最近から始めたものの、日々の業務を通じてスキルアップに励んでいます。
-</Text>
-<Text fontSize="lg" lineHeight="tall" mt={8}>
-このページでは、私が開発したプロジェクトや得意とするスキルについて紹介しています。
-ぜひご覧いただければ幸いです。
-</Text>
-</Box>
-</Box>
-</Flex>
-);
+  return (
+    <Box bg="gray.50" minH="100vh">
+      {/* サイトバー */}
+      <Box pos="fixed" top={0} left={0} w={16} h="100%" bg="gray.900">
+        <Flex h="100%" direction="column" justify="space-between" align="center" py={8}>
+          <Box>
+            <IconButton
+              icon={<FaBars color="white" />}
+              aria-label="Menu"
+              bg="transparent"
+              fontSize="2xl"
+              _hover={{ bg: "transparent" }}
+            />
+            <Image src="./images/logo.png" alt="Logo" w={10} h={10} mt={8} />
+          </Box>
+          <Box>
+            <Link href={`tel:${'08063809879'}`}>
+              <IconButton
+                icon={<FaPhone color="white" />}
+                aria-label="Phone"
+                bg="transparent"
+                fontSize="2xl"
+                _hover={{ bg: "transparent" }}
+                mr={2}
+              />
+            </Link>
+            <Link href={`https://www.instagram.com/${'kakkun.style'}`}>
+              <IconButton
+                icon={<FiInstagram color="white" />}
+                aria-label="Instagram"
+                bg="transparent"
+                fontSize="2xl"
+                _hover={{ bg: "transparent" }}
+                ml={2}
+              />
+            </Link>
+          </Box>
+        </Flex>
+      </Box>
+      {/* 自己紹介 */}
+      <Flex justify="center" alignItems="center" pt={20} pb={10}>
+        <Box maxW="md" borderWidth="1px" borderRadius="xl" overflow="hidden" boxShadow="xl" bg="white">
+          <Box bg="teal.500" h={24} display="flex" alignItems="center" justifyContent="center">
+            <Text fontSize="2xl" fontWeight="bold" color="white">About me</Text>
+          </Box>
+          <Box p={8}>
+            <Heading as="h1" size="3xl" mb={8} color="teal.500" textAlign="center" fontFamily="Georgia, serif">Katsunari Ootomo</Heading>
+            <Text fontSize="xl" fontWeight="bold" mb={8} color="gray.500" textAlign="center">Web Developer</Text>
+            <Text fontSize="lg" lineHeight="tall" color="gray.600">
+              Hello, my name is Katsunari Ootomo. I'm a web developer working for a major foreign trading company.
+            </Text>
+            <Text fontSize="lg" lineHeight="tall" color="gray.600" mt={8}>
+              I specialize in front-end development using React and TypeScript, and have experience building various types of web applications, including business systems and e-commerce sites. I'm also interested in UI/UX design and constantly striving to improve my skills in this area.
+            </Text>
+            <Text fontSize="lg" lineHeight="tall" color="gray.600" mt={8}>
+              If you have any questions or inquiries, please don't hesitate to contact me. You can reach me at the phone number listed below or via email using the contact form on this website.
+            </Text>
+          </Box>
+          </Box>
+      </Flex>
+      {/* フッター */}
+      <Box bg="gray.900" py={4}>
+        <Flex justify="space-between" align="center" px={8} color="white">
+          <Text>© 2023 Katsunari Ootomo</Text>
+          <Box>
+            <Link href={`https://www.instagram.com/${'kakkun.style'}`}>
+              <IconButton
+                icon={<FiInstagram />}
+                aria-label="Instagram"
+                bg="transparent"
+                fontSize="2xl"
+                _hover={{ bg: "transparent" }}
+                mr={2}
+              />
+            </Link>
+            <Link href={`tel:${'08063809879'}`}>
+              <Text fontSize="xl" fontWeight="bold">
+                <FaPhone color="white" mr={2} />
+                080-6380-9879
+              </Text>
+            </Link>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
 };
-
 export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,34 +6,41 @@ const inter = Inter({ subsets: ["latin"] });
 
 function Home() {
   return (
-    <Box className={styles.container}>
+    <Box bg="gray.50" className={styles.container}>
       <Flex
         as="nav"
         align="center"
         justify="space-between"
         py={4}
         px={8}
-        bg="gray.100"
+        bg="white"
+        boxShadow="md"
       >
         <Box>
-          <Heading as="h1" size="lg" letterSpacing="-0.1rem">
-            かっくんのポートフォリオ
+          <Heading as="h1" size="lg" letterSpacing="-0.1rem" color="teal.500">
+            Ootomo開発本部
           </Heading>
         </Box>
         <Stack as="ul" direction="row" spacing={4} align="center">
           <Box as="li" listStyleType="none">
             <a href="#about" className={styles.navLink}>
-              About
+              <Text fontSize="lg" fontWeight="bold" color="gray.500">
+                About
+              </Text>
             </a>
           </Box>
           <Box as="li" listStyleType="none">
             <a href="#projects" className={styles.navLink}>
-              Projects
+              <Text fontSize="lg" fontWeight="bold" color="gray.500">
+                Projects
+              </Text>
             </a>
           </Box>
           <Box as="li" listStyleType="none">
             <a href="#contact" className={styles.navLink}>
-              Contact
+              <Text fontSize="lg" fontWeight="bold" color="gray.500">
+                Contact
+              </Text>
             </a>
           </Box>
         </Stack>
@@ -59,12 +66,12 @@ function Home() {
           </Box>
           <Box mx={8}>
             <Heading as="h1" size="xl" mb={2} className={styles.heading}>
-              かっくん
+              KatsunariOotomo
             </Heading>
-            <Text fontSize="xl" className={styles.profileBio}>
+            <Text fontSize="lg" className={styles.profileBio}>
               大手外資系商社で働くエンジニア。フロントエンド開発を中心に、業務システムの開発に従事している。
             </Text>
-            <Text fontSize="xl" className={styles.profileBio}>
+            <Text fontSize="lg" className={styles.profileBio}>
               React, Next.js, TypeScriptを得意とし、最近はUI/UXの改善にも力を入れている。
             </Text>
           </Box>
@@ -76,75 +83,115 @@ function Home() {
         className={styles.projectsSection}
         px={8}
         py={16}
+        bg="white"
+        boxShadow="md"
       >
         <Heading as="h2" size="xl" mb={8} className={styles.subheading}>
           プロジェクト
         </Heading>
         <Stack spacing={8}>
           <Box>
-            <Heading as="h3" size="lg" mb={2} className={styles.projectTitle}>
-              社内向け業務システムの開発
-            </Heading>
-            <Text fontSize="lg" className={styles.projectDescription}>
-              社員の効率的な業務遂行のため、社内システムの改修・新規開発を行いました。
-              フロントエンド開発にはReactとTypeScript、バックエンドにはNode.jsを使用しました。
-        従業員の出勤・勤怠管理、経費申請、業務報告書作成など、幅広い業務プロセスをカバーするシステムを構築しました。
-      </Text>
-      <Box>
-        <Heading as="h3" size="lg" mb={2} className={styles.projectTitle}>
-          オンラインショップの開発
-        </Heading>
-        <Text fontSize="lg" className={styles.projectDescription}>
-          ユーザーが商品を閲覧・購入できるオンラインショップを開発しました。
-          フロントエンド開発にはReact、バックエンド開発にはNode.jsとExpressを使用し、
-          決済処理にはStripe APIを導入しました。
-          商品検索やカート機能、注文履歴の確認など、一般的なECサイトに必要な機能を実装しました。
-        </Text>
-        </Box>
-      </Box>
-    </Stack>
-  </Box>
-
-  <Box
-    as="section"
-    id="contact"
-    className={styles.contactSection}
-    px={8}
-    py={16}
-  >
-    <Heading as="h2" size="xl" mb={8} className={styles.subheading}>
-      お問い合わせ
-    </Heading>
-    <Text fontSize="lg" mb={8} className={styles.contactDescription}>
-      ご質問やご要望があれば、お気軽にお問い合わせください。
-    </Text>
-    <Stack as="form" spacing={4} direction="column">
-      <Box>
-        <Input type="text" id="name" name="name" placeholder="お名前" required />
-      </Box>
-      <Box>
-        <Input type="email" id="email" name="email" placeholder="メールアドレス" required />
-      </Box>
-      <Box>
-        <Input as="textarea" id="message" name="message" placeholder="お問い合わせ内容" required />
-      </Box>
-      <Box>
-        <Input type="submit" value="送信" className={styles.submitButton} />
-      </Box>
-    </Stack>
-  </Box>
-
-  <Box as="footer" className={styles.footer} px={8} py={4}>
-    <Text fontSize="lg">
-      © {new Date().getFullYear()} かっくん. All rights reserved.
-    </Text>
-  </Box>
-    </Box>
-    
-    );
-}
-
-export default Home;
-
-
-
+            <Heading as="h3" size="lg" mb={2}
+             className={styles.projectTitle}
+             color="teal.500"
+           >
+             社内向け業務システムの開発
+           </Heading>
+           <Text fontSize="lg" className={styles.projectDescription}>
+             社員の効率的な業務遂行のため、社内システムの改修・新規開発を行いました。
+             フロントエンド開発にはReactとTypeScript、バックエンドにはNode.jsを使用しました。
+             従業員の出勤・勤怠管理、経費申請、業務報告書作成など、幅広い業務プロセスをカバーするシステムを構築しました。
+           </Text>
+         </Box>
+         <Box>
+           <Heading
+             as="h3"
+             size="lg"
+             mb={2}
+             className={styles.projectTitle}
+             color="teal.500"
+           >
+             オンラインショップの開発
+           </Heading>
+           <Text fontSize="lg" className={styles.projectDescription}>
+             ユーザーが商品を閲覧・購入できるオンラインショップを開発しました。
+             フロントエンド開発にはReact、バックエンド開発にはNode.jsとExpressを使用し、
+             決済処理にはStripe APIを導入しました。
+             商品検索やカート機能、注文履歴の確認など、一般的なECサイトに必要な機能を実装しました。
+           </Text>
+         </Box>
+       </Stack>
+       </Box>
+       
+       <Box
+         as="section"
+         id="contact"
+         className={styles.contactSection}
+         px={8}
+         py={16}
+         bg="white"
+         boxShadow="md"
+       >
+         <Heading as="h2" size="xl" mb={8} className={styles.subheading} color="teal.500">
+           お問い合わせ
+         </Heading>
+         <Text fontSize="lg" mb={8} className={styles.contactDescription}>
+           ご質問やご要望があれば、お気軽にお問い合わせください。
+         </Text>
+         <Stack as="form" spacing={4} direction="column">
+           <Box>
+             <Input
+               type="text"
+               id="name"
+               name="name"
+               placeholder="お名前"
+               required
+               size="lg"
+             />
+           </Box>
+           <Box>
+             <Input
+               type="email"
+               id="email"
+               name="email"
+               placeholder="メールアドレス"
+               required
+               size="lg"
+             />
+           </Box>
+           <Box>
+             <Input
+               as="textarea"
+               id="message"
+               name="message"
+               placeholder="お問い合わせ内容"
+               required
+               size="lg"
+               h="120px"
+             />
+           </Box>
+           <Box>
+             <Input
+               type="submit"
+               value="送信"
+               className={styles.submitButton}
+               bg="teal.500"
+               color="white"
+               fontWeight="bold"
+               _hover={{ bg: "teal.600" }}
+               _active={{ bg: "teal.700" }}
+               size="lg"
+               />
+               </Box>
+               </Stack>
+               </Box>
+               
+               <Box as="footer" className={styles.footer} px={8} py={4} bg="gray.200">
+                 <Text fontSize="lg" color="gray.500">
+                   © {new Date().getFullYear()} かっくん. All rights reserved.
+                 </Text>
+               </Box>
+               </Box>
+               );
+               }
+               export default Home;
